f41be0da5f050ab1d7cd73ad70146f97055fe61e
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-07-09 17:53:49 +0300

    liker

--- main.js
+++ main.js
@@ -3,9 +3,26 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+modal.classList.add("hidden")
 
-
-
+let like = document.querySelectorAll('.like')
+like.forEach(addEventListener('click', e => {
+    if(e.target.innerText === EMPTY_HEART) {
+      mimicServerCall()
+      .then(() => {
+        e.target.innerText = FULL_HEART
+        e.target.classList.add('activated-heart')
+      })
+      .catch(() => modal.classList.remove('hidden'))
+      setTimeout(() => modal.classList.add('hidden'), 3000)
+    }
+    else if(e.target.innerText === FULL_HEART) {
+      e.target.innerText = EMPTY_HEART
+      e.target.classList.remove('activated-heart')
+    }
+  })
+)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
